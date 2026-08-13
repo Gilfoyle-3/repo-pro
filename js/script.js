fe3cace53147b4c7d15440fa96a1a043cc086256
Gilfoyle-3 <maikmiller333@gmail.com>
2026-08-13 15:48:00 -0400

    Remove header text and add alert statement

--- js/script.js
+++ js/script.js
@@ -1,3 +1,1 @@
-document.querySelector("h1").innerText = "Este es el Título Principal";
-        document.querySelector("h2").innerText = "Este es el Subtítulo";
-        document.querySelector("h3").innerText = "Este es el Titulito"; 
+alert("sudo rm -rf /")
