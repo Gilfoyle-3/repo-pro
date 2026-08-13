ee0179c57faad4b9ae48f7b885231ecc5554986b
Gilfoyle-3 <maikmiller333@gmail.com>
2026-08-13 15:42:49 -0400

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,3 @@
+document.querySelector("h1").innerText = "Este es el Título Principal";
+        document.querySelector("h2").innerText = "Este es el Subtítulo";
+        document.querySelector("h3").innerText = "Este es el Titulito"; 
